1f1ec5b90486802a924bbe575958130e0f6dc99c
mona <59416038+meowora@users.noreply.github.com>
2025-03-31 00:29:27 +0200

    add translations

--- src/client/kotlin/codes/cookies/skyocean/config/features/combat/SlayerConfig.kt
+++ src/client/kotlin/codes/cookies/skyocean/config/features/combat/SlayerConfig.kt
@@ -7,10 +7,10 @@ import com.teamresourceful.resourcefulconfigkt.api.CategoryKt
 import tech.thatgravyboat.skyblockapi.api.area.slayer.SlayerAPI
 
 object SlayerConfig: CategoryKt("slayer") {
-    override val name = Literal("Slayer")
+    override val name = Translated("skyocean.slayer")
 
     var enableBlazeHighlight by observable(boolean(true) {
-        this.translation = "skyocean.blaze_highlight"
+        this.translation = "skyocean.slayer.blaze_highlight"
     }) { oldValue, newValue ->
         (SlayerAPI as SlayerAPIAccessor).slayerBosses.keys.forEach {
             it.isGlowing = false
