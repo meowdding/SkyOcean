76e414c4de5720f1db26f27e80873cf9e8ae51f8
mona <59416038+meowora@users.noreply.github.com>
2025-04-09 20:37:55 +0200

    reintroduce red carpets :3

--- src/client/kotlin/codes/cookies/skyocean/events/BlockModelEvent.kt
+++ src/client/kotlin/codes/cookies/skyocean/events/BlockModelEvent.kt
@@ -1,11 +1,10 @@
 package codes.cookies.skyocean.events
 
-import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockState
 import tech.thatgravyboat.skyblockapi.api.events.base.CancellableSkyBlockEvent
 
-data class BlockModelEvent(var state: BlockState, val pos: BlockPos) : CancellableSkyBlockEvent() {
+data class BlockModelEvent(var state: BlockState) : CancellableSkyBlockEvent() {
     var block: Block
         get() = state.block
         set(value) {
