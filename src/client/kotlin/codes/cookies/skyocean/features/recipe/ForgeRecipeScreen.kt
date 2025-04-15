596006e3a3f9696bc39c745277f51acfae575423
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-15 18:17:13 +0200

    feat: client side invnetory
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/client/kotlin/codes/cookies/skyocean/features/recipe/ForgeRecipeScreen.kt
@@ -0,0 +1,27 @@
+package codes.cookies.skyocean.features.recipe
+
+import codes.cookies.skyocean.events.RegisterSkyOceanCommandEvent
+import codes.cookies.skyocean.helpers.ClientSideInventory
+import codes.cookies.skyocean.modules.Module
+import net.minecraft.world.item.Items
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+
+class ForgeRecipeScreen : ClientSideInventory(Text.of("Forge"), 6) {
+    init {
+        val items = List(6 * 9) { Items.BARRIER.defaultInstance }
+        addItems(items)
+    }
+
+    @Module
+    companion object {
+        @Subscription
+        fun onCommand(event: RegisterSkyOceanCommandEvent) {
+//             event.register("forge") {
+//                 callback {
+//                     McClient.setScreen(ForgeRecipeScreen())
+//                 }
+//             }
+        }
+    }
+}
