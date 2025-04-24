b2e73d143c5d88f38f9ac617f24455f9a08223bd
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-24 14:00:26 +0200

    fix: actual perk description, actual texture
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/codes/cookies/skyocean/features/misc/MinisterInCalendar.kt
+++ src/client/kotlin/codes/cookies/skyocean/features/misc/MinisterInCalendar.kt
@@ -4,18 +4,24 @@ import codes.cookies.skyocean.SkyOcean
 import codes.cookies.skyocean.config.features.misc.MiscConfig
 import codes.cookies.skyocean.utils.ChatUtils
 import codes.cookies.skyocean.utils.Utils.split
+import com.mojang.authlib.GameProfile
+import com.mojang.authlib.properties.Property
 import me.owdding.ktmodules.Module
+import net.minecraft.core.component.DataComponents
 import net.minecraft.world.item.Items
+import net.minecraft.world.item.component.ResolvableProfile
 import tech.thatgravyboat.skyblockapi.api.area.hub.ElectionAPI
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
 import tech.thatgravyboat.skyblockapi.api.item.replaceVisually
+import tech.thatgravyboat.skyblockapi.api.remote.RepoMobsAPI
 import tech.thatgravyboat.skyblockapi.impl.tagkey.ItemTag
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextBuilder.append
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.strikethrough
+import java.util.*
 
 @Module
 object MinisterInCalendar {
@@ -32,9 +38,19 @@ object MinisterInCalendar {
             return
         }
         val minister = ElectionAPI.currentMinister ?: return
+        val texture = RepoMobsAPI.getMobOrNull("${minister.name}_MAYOR")?.texture ?: return
 
         event.item.replaceVisually {
-            item = Items.PLAYER_HEAD // TODO: actual player head
+            item = Items.PLAYER_HEAD
+
+            set(
+                DataComponents.PROFILE,
+                ResolvableProfile(
+                    GameProfile(UUID.randomUUID(), "a").apply {
+                        this.properties.put("textures", Property("textures", texture))
+                    },
+                ),
+            )
 
             name(
                 Text.of {
@@ -56,10 +72,7 @@ object MinisterInCalendar {
 
                 minister.activePerks.forEach {
                     add(it.perkName) { color = TextColor.ORANGE }
-                    val a =
-                        "All Garden Visitors reward §aFine Flour§7 and §c+10% Copper §7for accepting offers. Invites §a5 §7unique visitors to your §aGarden §7and makes higher rarity visitors §a20% §7more likely to show up."
-                    // todo: actual description
-                    a.split(" ", 140).forEach {
+                    it.description.split(" ", 140).forEach {
                         add(it) { color = TextColor.GRAY }
                     }
                 }
