93e61ab5cfc5e33f1090e89aa55d76c8fb1820aa
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-03 22:26:54 +0200

    feat: piggy bank repair helper (#32)
    * feat: piggy bank repair helper
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * enus
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/client/kotlin/me/owdding/skyocean/features/chat/PiggyBankRepairHelper.kt
@@ -0,0 +1,28 @@
+package me.owdding.skyocean.features.chat
+
+import me.owdding.ktmodules.Module
+import me.owdding.skyocean.config.features.chat.ChatConfig
+import me.owdding.skyocean.utils.ChatUtils.sendWithPrefix
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.command
+
+@Module
+object PiggyBankRepairHelper {
+
+    private val regex = "You died(?: and your piggy bank cracked!|, lost [\\d.,]* coins and your piggy bank broke!)".toRegex()
+
+    @Subscription
+    fun onChat(event: ChatReceivedEvent.Pre) {
+        if (!ChatConfig.piggyRepairHelper) return
+        if (!regex.matches(event.text)) return
+
+        McClient.tell {
+            Text.of("Click here get 8 Pork from your sacks.") {
+                command = "/gfs ENCHANTED_PORK 8"
+            }.sendWithPrefix()
+        }
+    }
+}
