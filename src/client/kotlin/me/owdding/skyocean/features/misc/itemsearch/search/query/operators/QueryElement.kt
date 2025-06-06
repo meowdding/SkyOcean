5d9a1eaca05bc9c1835f5cb4af4cbf067bf66da9
Mona <59416038+meowora@users.noreply.github.com>
2025-06-06 17:36:51 +0200

    remove query stuff, will add this with a future update

--- src/client/kotlin/me/owdding/skyocean/features/misc/itemsearch/search/query/operators/QueryElement.kt
+++
@@ -1,7 +0,0 @@
-package me.owdding.skyocean.features.misc.itemsearch.search.query.operators
-
-import net.minecraft.world.item.ItemStack
-import java.util.function.Predicate
-
-interface πQueryElement : QueryOperator, Predicate<ItemStack> {
-}
--- src/client/kotlin/me/owdding/skyocean/features/misc/itemsearch/search/query/operators/QueryOperator.kt
+++
@@ -1,5 +0,0 @@
-package me.owdding.skyocean.features.misc.itemsearch.search.query.operators
-
-interface QueryOperator {
-
-}
