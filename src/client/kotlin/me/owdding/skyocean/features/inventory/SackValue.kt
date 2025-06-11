8f4018b682a395925ba99f1805fbcd9a52b08160
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-11 13:44:53 +0200

    stpid fucking feature
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/me/owdding/skyocean/features/inventory/SackValue.kt
+++ src/client/kotlin/me/owdding/skyocean/features/inventory/SackValue.kt
@@ -1,22 +1,24 @@
 package me.owdding.skyocean.features.inventory
 
+import earth.terrarium.olympus.client.components.base.BaseParentWidget
 import me.owdding.ktmodules.Module
-import me.owdding.lib.builder.DisplayFactory
 import me.owdding.lib.builder.LayoutFactory
 import me.owdding.lib.builder.MIDDLE
-import me.owdding.lib.displays.Display
 import me.owdding.lib.displays.Displays
-import me.owdding.lib.displays.withPadding
 import me.owdding.lib.extensions.shorten
+import me.owdding.lib.layouts.asWidget
 import me.owdding.skyocean.SkyOcean
 import me.owdding.skyocean.features.item.search.screen.ItemSearchScreen.asScrollable
 import me.owdding.skyocean.utils.ChatUtils
 import me.owdding.skyocean.utils.Utils.unaryMinus
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.components.AbstractWidget
+import net.minecraft.client.gui.layouts.LayoutElement
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.entity.player.Inventory
 import tech.thatgravyboat.skyblockapi.api.area.hub.BazaarAPI
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
-import tech.thatgravyboat.skyblockapi.api.events.base.predicates.InventoryTitle
-import tech.thatgravyboat.skyblockapi.api.events.render.RenderScreenForegroundEvent
 import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
 import tech.thatgravyboat.skyblockapi.api.profile.items.sacks.SacksAPI
 import tech.thatgravyboat.skyblockapi.api.remote.RepoItemsAPI
@@ -26,66 +28,83 @@ import tech.thatgravyboat.skyblockapi.utils.extentions.right
 import tech.thatgravyboat.skyblockapi.utils.extentions.top
 import tech.thatgravyboat.skyblockapi.utils.text.TextBuilder.append
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
 @Module
 object SackValue {
 
-    private var gold = 0xfc6f03
-
-    private var display: Display? = null
-
+    private val gold = 0xfc6f03
     private val regex = ".* Sack".toRegex()
+    private var oldWidget: AbstractWidget? = null
 
     @Subscription(priority = 1)
     fun onInvChange(event: InventoryChangeEvent) {
-        if (!regex.matches(event.title)) {
-            display = null
-            return
-        }
+        if (!regex.matches(event.screen.title.stripped)) return
+        if (event.isSkyBlockFiller || event.isInBottomRow) return
 
-        val height = event.screen.containerHeight
+        val screen = event.screen
+        val height = screen.containerHeight
 
-        display = Displays.background(
+        val display = BackgroundWidget(
             SkyOcean.id("blank"),
-            DisplayFactory.vertical {
-                val ids = event.itemStacks.mapNotNull { it.getSkyBlockId() }
+            LayoutFactory.vertical {
+                val ids = screen.menu.slots.filter { it.container !is Inventory }.mapNotNull { it.item.getSkyBlockId() }
                 val sackEntries = SacksAPI.sackItems.filter { it.key in ids }
 
-                horizontal {
+                val title = LayoutFactory.horizontal {
                     string(ChatUtils.ICON_SPACE_COMPONENT)
                     string(-"inventory.sack_value")
                 }
+                widget(title)
 
-                sackEntries.forEach { (item, amount) ->
-                    LayoutFactory.horizontal(alignment = MIDDLE) {
-                        display(Displays.item(RepoItemsAPI.getItem(item)))
-                        textDisplay(" - ${amount.shorten()}") {
-                            color = TextColor.DARK_GRAY
-                            append(" (") {
+                LayoutFactory.vertical {
+                    sackEntries.forEach { (item, amount) ->
+                        horizontal(alignment = MIDDLE) {
+                            display(Displays.item(RepoItemsAPI.getItem(item)))
+                            textDisplay(" - ${amount.shorten()}") {
                                 color = TextColor.DARK_GRAY
-                            }
-                            append(BazaarAPI.getProduct(item)?.sellPrice?.shorten() ?: "0") {
-                                color = gold
-                            }
-                            append(")") {
-                                color = TextColor.DARK_GRAY
+                                append(" (") {
+                                    color = TextColor.DARK_GRAY
+                                }
+                                append(BazaarAPI.getProduct(item)?.sellPrice?.shorten() ?: "0") {
+                                    color = gold
+                                }
+                                append(")") {
+                                    color = TextColor.DARK_GRAY
+                                }
                             }
                         }
-                    }.let {
-                        widget(it.asScrollable(it.width, height - 10))
                     }
+                }.let {
+                    widget(it.asScrollable(it.width + 10, height - 10 - title.height))
                 }
+            },
+            padding = 5,
+        ).apply { this.setPosition(screen.right + 5, screen.top) }
 
-            }.withPadding(5),
-        )
+        oldWidget?.let { screen.removeWidget(it) }
+        oldWidget = display
+        event.screen.addRenderableWidget(display)
+    }
+}
+
+class BackgroundWidget(val background: ResourceLocation, widget: LayoutElement, val padding: Int = 0) : BaseParentWidget() {
+    val body = widget.asWidget()
+
+    init {
+        this.addRenderableWidget(this.body)
     }
 
-    @Subscription
-    @InventoryTitle(".* Sack")
-    fun onRender(event: RenderScreenForegroundEvent) {
-        val container = event.screen as? AbstractContainerScreen<*> ?: return
-        display?.render(event.graphics, container.right + 5, container.top)
-    }
+    override fun getWidth() = body.width + padding * 2
+    override fun getHeight() = body.height + padding * 2
 
+    override fun renderWidget(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float) {
+        graphics.blitSprite(RenderType::guiTextured, background, this.x, this.y, body.width + padding * 2, body.height + padding * 2)
+
+        body.setPosition(this.x + padding, this.y + padding)
+        //body.render(graphics, mouseX, mouseY, partialTicks)
+
+        super.renderWidget(graphics, mouseX, mouseY, partialTicks)
+    }
 }
