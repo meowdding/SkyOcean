082ac13a754b116813eebf1f6e2215c846a73aa9
Mayaqq <i@mayaqq.dev>
2025-06-16 22:31:34 +0200

    Inventory Buttons (No Config menu yet)

---
+++ src/client/kotlin/me/owdding/skyocean/features/misc/buttons/InvButton.kt
@@ -0,0 +1,105 @@
+package me.owdding.skyocean.features.misc.buttons
+
+import earth.terrarium.olympus.client.components.base.renderer.WidgetRenderer
+import earth.terrarium.olympus.client.components.base.renderer.WidgetRendererContext
+import earth.terrarium.olympus.client.components.buttons.Button
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Items
+import tech.thatgravyboat.skyblockapi.api.remote.RepoItemsAPI
+import tech.thatgravyboat.skyblockapi.utils.extentions.pushPop
+import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+import kotlin.jvm.optionals.getOrNull
+
+class InvButton(val item: String, val index: Int, val bottom: Boolean, val screen: AbstractContainerScreen<*>, val title: String) : Button() {
+    var highlight = false
+    fun renderButtons(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float) {
+        graphics.pushPop {
+            val sprite = if (bottom) {
+                SELECTED_BOTTOM_TABS[index]
+            } else {
+                SELECTED_TOP_TABS[index]
+            }
+            renderPrevious(graphics, mouseX, mouseY, partialTicks, sprite)
+            val itemX = this@InvButton.width / 2 - 8 + this@InvButton.x
+            val itemY = if (bottom) {
+                this@InvButton.height  + this@InvButton.y - (this@InvButton.width / 2) - 8
+            } else {
+                this@InvButton.width / 2 - 8 + this@InvButton.y
+            }
+            val stack = if (item.contains(":")) {
+                BuiltInRegistries.ITEM.get(ResourceLocation.bySeparator(item, ':'))?.getOrNull()?.value()?.defaultInstance ?: Items.BARRIER.defaultInstance
+            } else {
+                RepoItemsAPI.getItem(item)
+            }
+            graphics.renderItem(stack, itemX, itemY)
+        }
+    }
+    override fun renderWidget(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
+        this.highlight = screen.title.stripped.contains(Regex(title))
+        return
+    }
+
+    fun renderPrevious(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float, sprite: ResourceLocation) {
+        graphics.blitSprite(
+            RenderType::guiOpaqueTexturedBackground,
+            sprite,
+            26, 32,
+            0, 0,
+            this.x, this.y,
+            getWidth(), getHeight() - 4
+        )
+        graphics.blitSprite(
+            RenderType::guiOpaqueTexturedBackground,
+            sprite,
+            26, 32,
+            0, 32-4,
+            this.x, this.y + getHeight() - 4,
+            getWidth(), 4
+        )
+
+        WidgetRenderer.empty<Button>().render(graphics, WidgetRendererContext(this, mouseX, mouseY), partialTick)
+    }
+
+    companion object {
+        val UNSELECTED_TOP_TABS = arrayOf<ResourceLocation>(
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_1"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_2"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_3"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_4"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_5"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_6"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_unselected_7"),
+        )
+        val SELECTED_TOP_TABS = arrayOf<ResourceLocation>(
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_1"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_2"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_3"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_4"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_5"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_6"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_top_selected_7"),
+        )
+        val UNSELECTED_BOTTOM_TABS = arrayOf<ResourceLocation>(
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_1"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_2"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_3"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_4"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_5"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_6"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_unselected_7"),
+        )
+        val SELECTED_BOTTOM_TABS = arrayOf<ResourceLocation>(
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_1"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_2"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_3"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_4"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_5"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_6"),
+            ResourceLocation.withDefaultNamespace("container/creative_inventory/tab_bottom_selected_7"),
+        )
+    }
+}
---
+++ src/client/kotlin/me/owdding/skyocean/features/misc/buttons/InvButtons.kt
@@ -0,0 +1,77 @@
+package me.owdding.skyocean.features.misc.buttons
+
+import me.owdding.ktmodules.Module
+import me.owdding.skyocean.config.features.misc.Buttons
+import net.fabricmc.fabric.api.client.screen.v1.Screens
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
+import tech.thatgravyboat.skyblockapi.api.events.render.RenderScreenBackgroundEvent
+import tech.thatgravyboat.skyblockapi.api.events.render.RenderScreenForegroundEvent
+import tech.thatgravyboat.skyblockapi.api.events.screen.ScreenInitializedEvent
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.extentions.containerHeight
+import tech.thatgravyboat.skyblockapi.utils.extentions.containerWidth
+import tech.thatgravyboat.skyblockapi.utils.extentions.left
+import tech.thatgravyboat.skyblockapi.utils.extentions.top
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+
+@Module
+object InvButtons {
+
+    @Subscription
+    @OnlyOnSkyBlock
+    fun onScreen(event: ScreenInitializedEvent) {
+        if (event.screen !is AbstractContainerScreen<*>) return
+        val screen = event.screen as AbstractContainerScreen<*>
+
+        val buttonOffset = 4
+        val buttonHeight = 28
+
+        for (y in 0 until 2) {
+            for (x in 0 until 7) {
+                val posX = (x * (screen.containerWidth / 7)) + screen.left
+                val yOffset = if (y == 0) {
+                    -buttonHeight + buttonOffset
+                } else {
+                    -buttonOffset
+                }
+                val posY = (y * screen.containerHeight) + screen.top + yOffset
+
+                val button = Buttons.buttons[x + y * 7]
+                event.widgets.add(
+                    InvButton(button.item, x, y == 1, screen, button.title)
+                        .withSize(26, buttonHeight)
+                        .withCallback {
+                            val command = button.command.replace("/", "")
+                            McClient.connection?.sendCommand(command)
+                        }
+                        .withTooltip(Text.of(button.tooltip.takeIf { it.isNotEmpty() } ?: button.command))
+                        .withPosition(posX, posY)
+                )
+            }
+        }
+    }
+
+    @Subscription
+    @OnlyOnSkyBlock
+    fun onScreenBackground(event: RenderScreenBackgroundEvent) {
+        if (event.screen !is AbstractContainerScreen<*>) return
+        Screens.getButtons(event.screen).forEach {
+            if (it is InvButton && !it.isHovered && !it.highlight) {
+                it.renderButtons(event.graphics, 0, 0, 0F)
+            }
+        }
+    }
+
+    @Subscription
+    @OnlyOnSkyBlock
+    fun onScreenForeground(event: RenderScreenForegroundEvent) {
+        if (event.screen !is AbstractContainerScreen<*>) return
+        Screens.getButtons(event.screen).forEach {
+            if (it is InvButton && (it.isHovered || it.highlight)) {
+                it.renderButtons(event.graphics, 0, 0, 0F)
+            }
+        }
+    }
+}
