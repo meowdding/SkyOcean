ecc3e355f4103252e3c0b5888edea4760f061986
Mayaqq <73876072+MayaqqDev@users.noreply.github.com>
2025-06-18 22:38:18 +0200

    Update src/client/kotlin/me/owdding/skyocean/config/features/inventory/Buttons.kt
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/me/owdding/skyocean/config/features/inventory/Buttons.kt
+++ src/client/kotlin/me/owdding/skyocean/config/features/inventory/Buttons.kt
@@ -9,20 +9,20 @@ import org.intellij.lang.annotations.Language
 object Buttons : CategoryKt("buttons") {
     override val hidden: Boolean = true
 
-    val button0: ButtonConfig = obj("button0", ButtonConfig("minecraft:diamond_sword", "Skills", "^Your Skills$"))
-    val button1: ButtonConfig = obj("button1", ButtonConfig("minecraft:painting", "Collections",  "^Collections$"))
+    val button0: ButtonConfig = obj("button0", ButtonConfig("minecraft:diamond_sword", "Skills", "Your Skills"))
+    val button1: ButtonConfig = obj("button1", ButtonConfig("minecraft:painting", "Collections",  "Collections"))
     val button2: ButtonConfig = obj("button2", ButtonConfig("minecraft:bone", "Pets", "Pets(?: \\(\\d+/\\d+\\))?"))
-    val button3: ButtonConfig = obj("butto3", ButtonConfig("ARMOR_OF_YOG_CHESTPLATE", "Wardrobe", "^Wardrobe(?: \\(\\d+/\\d+\\))?"))
-    val button4: ButtonConfig = obj("button4", ButtonConfig("minecraft:bundle", "Sacks", "^Sack of Sacks$"))
-    val button5: ButtonConfig = obj("button5", ButtonConfig("RUNEBOOK", "Accessories", "^Accessory Bag(?: \\(\\d+/\\d+\\))?"))
-    val button6: ButtonConfig = obj("button6", ButtonConfig("minecraft:ender_chest", "Storage", "^Storage$"))
+    val button3: ButtonConfig = obj("butto3", ButtonConfig("ARMOR_OF_YOG_CHESTPLATE", "Wardrobe", "Wardrobe(?: \\(\\d+/\\d+\\))?"))
+    val button4: ButtonConfig = obj("button4", ButtonConfig("minecraft:bundle", "Sacks", "Sack of Sacks"))
+    val button5: ButtonConfig = obj("button5", ButtonConfig("RUNEBOOK", "Accessories", "Accessory Bag(?: \\(\\d+/\\d+\\))?"))
+    val button6: ButtonConfig = obj("button6", ButtonConfig("minecraft:ender_chest", "Storage", "Storage"))
     val button7: ButtonConfig = obj("button7", ButtonConfig("minecraft:grass_block", "warp island", "a^", "Island"))
     val button8: ButtonConfig = obj("button8", ButtonConfig("HUB_PORTAL", "Hub", "a^"))
     val button9: ButtonConfig = obj("button9", ButtonConfig("minecraft:skeleton_skull", "Dungeons", "a^"))
-    val button10: ButtonConfig = obj("button10", ButtonConfig("SMOOTH_CHOCOLATE_BAR", "ChocolateFactory", "^Chocolate Factory$", "Chocolate Factory"))
-    val button11: ButtonConfig = obj("button11", ButtonConfig("ESSENCE_GOLD", "Bazaar", "^(Special )?Bazaar$"))
-    val button12: ButtonConfig = obj("button12", ButtonConfig("ESSENCE_DIAMOND", "Auction", "^(Co-op )?Auction House$"))
-    val button13: ButtonConfig = obj("button13", ButtonConfig("minecraft:crafting_table", "CraftingTable", "^Craft Item$", "Crafting Table"))
+    val button10: ButtonConfig = obj("button10", ButtonConfig("SMOOTH_CHOCOLATE_BAR", "ChocolateFactory", "Chocolate Factory", "Chocolate Factory"))
+    val button11: ButtonConfig = obj("button11", ButtonConfig("ESSENCE_GOLD", "Bazaar", "(?:Special )?Bazaar"))
+    val button12: ButtonConfig = obj("button12", ButtonConfig("ESSENCE_DIAMOND", "Auction", "(?:Co-op )?Auction House"))
+    val button13: ButtonConfig = obj("button13", ButtonConfig("minecraft:crafting_table", "CraftingTable", "Craft Item", "Crafting Table"))
 
     val buttons get() = arrayOf(button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13)
 
