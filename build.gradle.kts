11dbe27e99e49fa275810a18b43067caf42a4396
Mona <59416038+meowora@users.noreply.github.com>
2025-06-24 16:54:31 +0200

    add depends to mod.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,7 +130,19 @@ tasks.processResources {
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
 
     filesMatching(listOf("fabric.mod.json")) {
-        expand("version" to project.version)
+        expand(
+            "version" to project.version,
+            "minecraft" to libs.versions.minecraft.get(),
+            "fabricLoader" to libs.versions.fabric.loader.get(),
+            "fabricLanguageKotlin" to libs.versions.fabric.language.kotlin.get(),
+            "meowddingLib" to libs.versions.meowdding.lib.get(),
+            "resourcefullib" to libs.versions.rlib.get(),
+            "skyblockApi" to libs.versions.skyblockapi.get(),
+            "olympus" to libs.versions.olympus.get(),
+            "placeholderApi" to libs.versions.placeholders.get(),
+            "resourcefulconfigkt" to libs.versions.rconfigkt.get(),
+            "resourcefulconfig" to libs.versions.rconfig.get(),
+        )
     }
 }
 
