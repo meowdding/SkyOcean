6c4e3807c758731e2721c2d4094c6fbfba7dffdb
Empa <42304516+ItsEmpa@users.noreply.github.com>
2025-07-12 21:25:40 +0200

    feat: museum item source (#57)
    Co-authored-by: Empa <itsempa@users.noreply.github.com>

---
+++ src/client/kotlin/me/owdding/skyocean/features/item/search/soures/MuseumItemSource.kt
@@ -0,0 +1,30 @@
+package me.owdding.skyocean.features.item.search.soures
+
+import me.owdding.skyocean.features.item.search.ItemContext
+import me.owdding.skyocean.features.item.search.item.SimpleTrackedItem
+import net.minecraft.network.chat.Component
+import tech.thatgravyboat.skyblockapi.api.profile.items.museum.MuseumAPI
+import tech.thatgravyboat.skyblockapi.api.profile.items.museum.MuseumCategory
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+
+object MuseumItemSource : ItemSource {
+    override fun getAll() = MuseumAPI.getItemsWithCategory().flatMap { (category, items) ->
+        items.map { SimpleTrackedItem(it, MuseumItemContext(category)) }
+    }
+
+    override val type = ItemSources.MUSEUM
+}
+
+// TODO: add highlighting categories and items in museum menu
+data class MuseumItemContext(private val category: MuseumCategory) : ItemContext {
+    override fun collectLines(): List<Component> = build {
+        add("Museum Category $category") { color = TextColor.GRAY }
+        add("Click to warp to museum!") { color = TextColor.YELLOW }
+    }
+
+    override fun open() = McClient.sendCommand("warp museum")
+
+    override val source = ItemSources.MUSEUM
+}
