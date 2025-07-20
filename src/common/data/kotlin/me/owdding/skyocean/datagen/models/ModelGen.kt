914b8b2b5f7a9137c32c4e0baadc1832003b82b5
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-20 14:38:21 +0200

    feat: multi version (#61)
    * stuff
    
    * stuff
    
    * meow
    
    * grrrr
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * windows my beloved
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: 1.21.8 & shader
    
    * fake blocks
    
    * waypoints
    
    * feat: datagen
    
    * bump api & mlib
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: remaining stuff
    
    * fix initial focus of text box
    
    * bumping
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * reset search and category on close
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: item value screen using api v1 stuff
    
    * changes
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: Gravy Boat <gravy@thatgravyboat.tech>
    Co-authored-by: Mona <59416038+meowora@users.noreply.github.com>

---
+++ src/common/data/kotlin/me/owdding/skyocean/datagen/models/ModelGen.kt
@@ -0,0 +1,61 @@
+package me.owdding.skyocean.datagen.models
+
+import me.owdding.skyocean.datagen.models.factories.DefaultModelFactory
+import me.owdding.skyocean.datagen.models.factories.GlassPaneFactory
+import me.owdding.skyocean.datagen.models.factories.InfestedStoneFactory
+import me.owdding.skyocean.datagen.models.factories.SnowLayerFactory
+import me.owdding.skyocean.events.RegisterFakeBlocksEvent
+import me.owdding.skyocean.helpers.BLOCK_STATES_PATH
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.minecraft.client.data.models.BlockModelGenerators
+import net.minecraft.client.data.models.ItemModelGenerators
+import net.minecraft.data.CachedOutput
+import net.minecraft.data.PackOutput
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.block.Block
+import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI
+import java.util.concurrent.CompletableFuture
+
+class ModelGen(output: FabricDataOutput) : FabricModelProvider(output) {
+    val blockStatePathProvider = output.createPathProvider(PackOutput.Target.RESOURCE_PACK, BLOCK_STATES_PATH)
+    val fakeBlockStateCollector = FakeBlockStateCollector(mutableListOf())
+    val context = ModelGenContext(fakeBlockStateCollector, output)
+
+    val factories = listOf(
+        SnowLayerFactory,
+        GlassPaneFactory,
+        InfestedStoneFactory,
+        DefaultModelFactory,
+    )
+
+    override fun run(output: CachedOutput): CompletableFuture<*> {
+        return CompletableFuture.allOf(super.run(output), fakeBlockStateCollector.save(output, blockStatePathProvider))
+    }
+
+    override fun generateBlockStateModels(blockStateModelGenerator: BlockModelGenerators) {
+        factories.forEach { it.generator = blockStateModelGenerator }
+
+        val fakeBlocks = mutableMapOf<Block, MutableList<ResourceLocation>>()
+        fun register(block: Block, definition: ResourceLocation) {
+            fakeBlocks.getOrPut(block, ::mutableListOf).add(definition)
+        }
+        RegisterFakeBlocksEvent { block, definition, predicate ->
+            register(block, definition)
+            println("Registering $block")
+        }.post(SkyBlockAPI.eventBus)
+        println("meow")
+        fakeBlocks.entries.forEach { (block, entries) ->
+            factories.firstOrNull { it.isFor(block) }?.let {
+                entries.forEach { model ->
+                    println("Creating $model")
+                    it.create(block, model, blockStateModelGenerator, context)
+                }
+            }
+        }
+    }
+
+    override fun generateItemModels(itemModelGenerator: ItemModelGenerators) {
+
+    }
+}
