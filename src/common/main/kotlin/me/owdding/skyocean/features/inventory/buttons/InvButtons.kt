46465df811139edde8b9148cf6bd10fabd6182e3
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-28 20:15:56 +0200

    fix: inv buttons config not configing
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyocean/features/inventory/buttons/InvButtons.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/inventory/buttons/InvButtons.kt
@@ -26,7 +26,7 @@ import tech.thatgravyboat.skyblockapi.utils.text.Text
 object InvButtons {
     @Subscription
     fun onScreen(event: ScreenInitializedEvent) {
-        if (shouldHideButtons(event.screen)) return
+        if (!shouldShowButtons(event.screen)) return
         val screen = event.screen as AbstractContainerScreen<*>
 
         val buttonOffset = 8
@@ -63,7 +63,7 @@ object InvButtons {
 
     @Subscription
     fun onScreenBackground(event: RenderScreenBackgroundEvent) {
-        if (shouldHideButtons(event.screen)) return
+        if (!shouldShowButtons(event.screen)) return
         Screens.getButtons(event.screen).forEach {
             if (it is InvButton && !it.highlight) {
                 it.renderButtons(event.graphics, 0, 0, 0F)
@@ -73,7 +73,7 @@ object InvButtons {
 
     @Subscription
     fun onScreenForeground(event: RenderScreenForegroundEvent) {
-        if (shouldHideButtons(event.screen)) return
+        if (!shouldShowButtons(event.screen)) return
         Screens.getButtons(event.screen).forEach {
             if (it is InvButton && it.highlight) {
                 it.renderButtons(event.graphics, 0, 0, 0F)
@@ -92,10 +92,8 @@ object InvButtons {
         }
     }
 
-    private fun shouldHideButtons(screen: Screen): Boolean {
-        return screen !is AbstractContainerScreen<*> ||
-            !InventoryConfig.inventoryButtons ||
-            (!LocationAPI.isOnSkyBlock && screen !is ButtonConfigScreen)
+    private fun shouldShowButtons(screen: Screen): Boolean {
+        return screen is AbstractContainerScreen<*> && InventoryConfig.inventoryButtons && (LocationAPI.isOnSkyBlock || screen is ButtonConfigScreen)
     }
 
 }
