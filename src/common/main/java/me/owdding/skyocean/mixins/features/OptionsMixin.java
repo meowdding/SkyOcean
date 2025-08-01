03e9162218d63f34eeacb38c36ded1cf1d610b90
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-08-01 18:11:58 +0200

    wrapmethod
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/java/me/owdding/skyocean/mixins/features/OptionsMixin.java
+++ src/common/main/java/me/owdding/skyocean/mixins/features/OptionsMixin.java
@@ -1,21 +1,21 @@
 package me.owdding.skyocean.mixins.features;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.owdding.skyocean.config.features.misc.MiscConfig;
 import net.minecraft.client.CloudStatus;
 import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Options.class)
 public class OptionsMixin {
 
-    @ModifyReturnValue(method = "getCloudsType", at = @At("RETURN"))
-    private CloudStatus modifyCloudsType(CloudStatus original) {
+    @WrapMethod(method = "getCloudsType")
+    private CloudStatus modifyCloudsType(Operation<CloudStatus> original) {
         if (MiscConfig.INSTANCE.getShouldHideClouds()) {
             return CloudStatus.OFF;
         }
-        return original;
+        return original.call();
     }
 
 }
