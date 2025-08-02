a2a3dd08bde8a7f26880a4b90ee483964a4f668b
Mona <59416038+meowora@users.noreply.github.com>
2025-08-02 10:28:28 +0200

    fix: profile in chat not working (#73)
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyocean/features/chat/ProfileInChat.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/chat/ProfileInChat.kt
@@ -1,50 +1,78 @@
 package me.owdding.skyocean.features.chat
 
+import com.google.common.cache.Cache
+import com.google.common.cache.CacheBuilder
 import me.owdding.ktmodules.Module
 import me.owdding.skyocean.config.features.chat.ChatConfig
+import me.owdding.skyocean.utils.ChatUtils
+import me.owdding.skyocean.utils.Utils.get
+import me.owdding.skyocean.utils.Utils.set
+import me.owdding.skyocean.utils.Utils.visitSiblings
+import net.minecraft.network.chat.CommonComponents
 import net.minecraft.network.chat.Component
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription.Companion.LOWEST
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
 import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent
 import tech.thatgravyboat.skyblockapi.api.events.info.TabListChangeEvent
 import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.Text.send
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.hover
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toJavaDuration
 
 @Module
 object ProfileInChat {
 
-    val usernameToProfileTypeMap = mutableMapOf<String, Component>()
+    private val profileTypes = listOf("♲", "Ⓑ", "☀")
+    private val usernameToProfileTypeCache: Cache<String, Component> = CacheBuilder.newBuilder()
+        .maximumSize(100)
+        .expireAfterAccess(10.minutes.toJavaDuration())
+        .expireAfterWrite(10.minutes.toJavaDuration())
+        .build()
 
-    @Subscription
+    @Subscription(TabListChangeEvent::class)
     @OnlyOnSkyBlock
-    fun check(event: TabListChangeEvent) {
+    fun onTablistUpdate() {
+        if (!ChatConfig.enableProfileInChat) return
         McClient.players.forEach { player ->
             val name = player.profile.name
             val suffix = player.team?.playerSuffix ?: return@forEach
-            suffix.siblings.forEach { sibling ->
-                if (sibling.stripped in listOf("♲", "Ⓑ", "☀")) {
-                    usernameToProfileTypeMap[name] = sibling
+            suffix.visitSiblings { sibling ->
+                if (sibling.stripped.trim() in profileTypes) {
+                    usernameToProfileTypeCache[name] = Text.of {
+                        append(sibling)
+                        append(CommonComponents.SPACE)
+                        hover = Text.of {
+                            append(ChatUtils.ICON_SPACE_COMPONENT)
+                            append("Added by SkyOcean!")
+                            this.color = TextColor.GRAY
+                        }
+                    }
                 }
             }
         }
     }
 
-    @Subscription
     @OnlyOnSkyBlock
+    @Subscription(priority = LOWEST)
     fun onChat(event: ChatReceivedEvent.Pre) {
         if (!ChatConfig.enableProfileInChat) return
         val index = event.component.siblings.indexOfFirst { sibling -> sibling.stripped.startsWith(": ") } - 1
         if (index < 0) return
-        var name = event.component.siblings[index].stripped
-        if (name.startsWith("] ")) {
-            name = name.replace("] ", "")
-        }
-        val profileType = usernameToProfileTypeMap[name] ?: return
+        val name = event.component.siblings[index].stripped.trim().substringAfterLast(" ")
+        val targetIndex = if (name.equals(McPlayer.name, true)) {
+            event.component.siblings.indexOfFirst { sibling -> sibling.style.hoverEvent == null }
+        } else index
+        val profileType = usernameToProfileTypeCache[name] ?: return
         event.cancel()
         val modified = event.component.copy()
-        modified.siblings.add(index + 1, Text.of(" ").append(profileType))
+        modified.siblings.add(targetIndex, profileType)
         modified.send()
     }
 }
