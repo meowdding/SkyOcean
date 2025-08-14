1e16d8a6dec2151886fee8adee38d7424efafcea
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-08-14 13:54:16 +0200

    feat: bobber alive time
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/common/main/kotlin/me/owdding/skyocean/features/fishing/BobberTime.kt
@@ -0,0 +1,32 @@
+package me.owdding.skyocean.features.fishing
+
+import me.owdding.ktmodules.Module
+import me.owdding.lib.utils.RenderUtils.renderTextInWorld
+import me.owdding.skyocean.config.features.fishing.FishingConfig
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
+import tech.thatgravyboat.skyblockapi.api.events.render.RenderWorldEvent
+import tech.thatgravyboat.skyblockapi.helpers.McFont
+import tech.thatgravyboat.skyblockapi.helpers.McPlayer
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+
+@Module
+object BobberTime {
+
+    @Subscription
+    @OnlyOnSkyBlock
+    fun onRender(event: RenderWorldEvent.AfterTranslucent) {
+        if (!FishingConfig.bobberTime) return
+        val rod = McPlayer.self?.fishing ?: return
+        val time = rod.tickCount / 20.0
+
+        event.renderTextInWorld(
+            rod.position().add(-0.5, -0.3, -0.5),
+            Text.of("${"%.2f".format(time)}s").withColor(TextColor.GRAY),
+            center = true,
+            yOffset = -McFont.height.toFloat(),
+        )
+    }
+
+}
