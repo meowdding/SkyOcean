1f0ec635f931d6bc9c4539a9d2b05edb3240a1ba
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-08-14 18:06:16 +0200

    feat: customize crafthelper location
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/display/CraftHelperLocation.kt
@@ -0,0 +1,44 @@
+package me.owdding.skyocean.features.recipe.crafthelper.display
+
+import me.owdding.skyocean.config.features.misc.MiscConfig
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+
+enum class CraftHelperLocation(val position: (Int, Int) -> Pair<Int, Int>) {
+    TOP_LEFT(
+        { width, height ->
+            margin to margin
+        },
+    ),
+    TOP_RIGHT(
+        { width, height ->
+            screenWidth - width - margin to margin
+        },
+    ),
+    BOTTOM_LEFT(
+        { width, height ->
+            margin to screenHeight - height - margin
+        },
+    ),
+    BOTTOM_RIGHT(
+        { width, height ->
+            screenWidth - width - margin to screenHeight - height - margin
+        },
+    ),
+    LEFT_OF_INVENTORY(
+        { width, height ->
+            margin to (screenHeight / 2 - height / 2)
+        },
+    ),
+    RIGHT_OF_INVENTORY(
+        { width, height ->
+            screenWidth - width - margin to (screenHeight / 2 - height / 2)
+        },
+    ),
+    ;
+
+    companion object {
+        private val screenWidth get() = McClient.window.guiScaledWidth
+        private val screenHeight get() = McClient.window.guiScaledHeight
+        private val margin get() = MiscConfig.craftHelperMargin
+    }
+}
