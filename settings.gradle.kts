433261124cbd69221988e15d7d085707f561b5a6
Mona <59416038+meowora@users.noreply.github.com>
2025-08-22 09:12:33 +0200

    meow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ rootProject.name = "SkyOcean"
 
 pluginManagement {
     repositories {
+        maven("https://maven.teamresourceful.com/repository/maven-private/")
         maven(url = "https://maven.teamresourceful.com/repository/maven-public/")
         maven(url = "https://maven.msrandom.net/repository/cloche")
         gradlePluginPortal()
