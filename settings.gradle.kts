d5b168d64f1e4805b571cb30cad007721c145cab
Mona <59416038+meowora@users.noreply.github.com>
2025-08-22 18:17:19 +0200

    feat: use meowdding gradle plugin (#90)
    * feat: use meowdding gradle plugin
    
    * meow
    
    * meow meow
    
    * change stuff :3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ rootProject.name = "SkyOcean"
 
 pluginManagement {
     repositories {
+        maven("https://maven.teamresourceful.com/repository/maven-private/")
         maven(url = "https://maven.teamresourceful.com/repository/maven-public/")
         maven(url = "https://maven.msrandom.net/repository/cloche")
         gradlePluginPortal()
@@ -23,7 +24,6 @@ dependencyResolutionManagement {
 
 buildscript {
     repositories {
-        mavenLocal()
         mavenCentral()
         gradlePluginPortal()
         maven("https://maven.teamresourceful.com/repository/maven-private/")
