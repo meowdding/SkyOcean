3b45c9f44930c985ca0fc81c654d3b8fa588c427
Mona <59416038+meowora@users.noreply.github.com>
2025-08-25 17:26:07 +0200

    meow :3

--- src/common/data/kotlin/me/owdding/skyocean/datagen/dispatcher/SkyOceanDataGenerator.kt
+++ src/common/data/kotlin/me/owdding/skyocean/datagen/dispatcher/SkyOceanDataGenerator.kt
@@ -2,10 +2,8 @@ package me.owdding.skyocean.datagen.dispatcher
 
 import me.owdding.skyocean.SkyOcean
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-import net.minecraft.SharedConstants
-import net.minecraft.data.DataGenerator
 
-data class SkyOceanDataGenerator(val parent: FabricDataGenerator) : DataGenerator(parent.rootOutputFolder, SharedConstants.getCurrentVersion(), true) {
+data class SkyOceanDataGenerator(val parent: FabricDataGenerator) {
     fun createPack(): FabricDataGenerator.Pack = parent.createPack()
 
     fun createResourcePack(name: String): FabricDataGenerator.Pack {
