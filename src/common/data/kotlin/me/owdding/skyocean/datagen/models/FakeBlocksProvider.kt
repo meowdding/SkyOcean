2181fea5acccccb80a617cfcb8520eb35adeab80
Mona <59416038+meowora@users.noreply.github.com>
2025-08-29 16:07:56 +0200

    fix: forgot that stairs aren't cubes

--- src/common/data/kotlin/me/owdding/skyocean/datagen/models/FakeBlocksProvider.kt
+++ src/common/data/kotlin/me/owdding/skyocean/datagen/models/FakeBlocksProvider.kt
@@ -24,6 +24,7 @@ class FakeBlocksProvider(output: FabricDataOutput, saveBlockStates: Boolean = tr
         SnowLayerFactory,
         GlassPaneFactory,
         WallModelFactory,
+        StairModelFactory,
         RemapFactory,
         DefaultModelFactory,
     )
