93d2d4fa3f8b23c82345365b405f38934a4190ea
Helium9 <80259183+sivthepolarfox@users.noreply.github.com>
2025-09-09 16:41:10 +0200

    feat: Add Set Crafthelper Keybind

--- src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/modifiers/RecipeModifier.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/modifiers/RecipeModifier.kt
@@ -1,8 +1,10 @@
 package me.owdding.skyocean.features.recipe.crafthelper.modifiers
 
+import com.mojang.blaze3d.platform.InputConstants
 import me.owdding.ktmodules.Module
 import me.owdding.skyocean.SkyOcean
 import me.owdding.skyocean.api.SkyOceanItemId
+import me.owdding.skyocean.config.SkyOceanKeybind
 import me.owdding.skyocean.config.features.misc.MiscConfig
 import me.owdding.skyocean.data.profile.CraftHelperStorage.setSelected
 import me.owdding.skyocean.utils.ChatUtils
@@ -12,18 +14,20 @@ import net.minecraft.world.item.Items
 import net.minecraft.world.item.component.TooltipDisplay
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.screen.ScreenKeyReleasedEvent
 import tech.thatgravyboat.skyblockapi.api.item.replaceVisually
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockapi.impl.tagkey.ItemTag
 import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
+import tech.thatgravyboat.skyblockapi.utils.extentions.getHoveredSlot
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
 @Module
 object RecipeModifier {
-
+    val SET_CRAFTHELPER_KEYBIND = SkyOceanKeybind("skyocean.crafthelper.keybind", InputConstants.KEY_V)
 
     @Subscription
     fun onInventoryChange(event: InventoryChangeEvent) {
@@ -55,6 +59,13 @@ object RecipeModifier {
                 McScreen.self?.let { it.resize(McClient.self, it.width, it.height) }
             }
         }
+    }
+
+    @Subscription
+    fun onKeybind(event: ScreenKeyReleasedEvent) {
+        if (!SET_CRAFTHELPER_KEYBIND.matches(event)) return
+        val item = McScreen.asMenu?.getHoveredSlot()?.item?.takeUnless { it.isEmpty } ?: return
+        setSelected(SkyOceanItemId.fromItem(item))
     }
 
 }
