d7e8ee743df82adb9dff6aabe025507035373e73
Mona <59416038+meowora@users.noreply.github.com>
2025-09-14 23:18:32 +0200

    grrr

--- src/common/main/kotlin/me/owdding/skyocean/utils/rendering/StyledItemWidget.kt
+++ src/common/main/kotlin/me/owdding/skyocean/utils/rendering/StyledItemWidget.kt
@@ -2,8 +2,9 @@ package me.owdding.skyocean.utils.rendering
 
 import earth.terrarium.olympus.client.components.base.BaseWidget
 import net.minecraft.world.item.ItemStack
+import net.msrandom.stub.Stub
 
+@Stub
 expect class StyledItemWidget : BaseWidget {
-
     constructor(stack: ItemStack)
 }
