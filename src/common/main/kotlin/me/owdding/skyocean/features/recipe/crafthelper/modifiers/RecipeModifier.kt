8740a0a0226166f817220cb24ace20426d5fab69
Siv <80259183+sivthepolarfox@users.noreply.github.com>
2025-09-14 23:48:32 +0200

    feat: Crafthelper set keybind (#105)
    * feat: Add Set Crafthelper Keybind
    
    * Revert "stuff"
    
    This reverts commit ff4b5b9a50ac48853eb5ce938b00093f9c7bd8ed.
    
    * Refresh
    
    * First rei test (no worky)
    
    * It works in item list now but recipe viewer still doesnt work :(
    
    * Ok this works
    
    * unneeded
    
    * Chat feedback
    
    * Uhh i think like this :3
    
    * stop build fail
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * add missing translation and rename skyocean.<>.keybind to skyocean.keybind.<>
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * Peak mona suggestion thanks mona @meowora
    
    Co-authored-by: Mona <59416038+meowora@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: Mona <59416038+meowora@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/modifiers/RecipeModifier.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/modifiers/RecipeModifier.kt
@@ -1,10 +1,14 @@
 package me.owdding.skyocean.features.recipe.crafthelper.modifiers
 
+import com.mojang.blaze3d.platform.InputConstants
 import me.owdding.ktmodules.Module
+import me.owdding.lib.compat.REIRuntimeCompatability
 import me.owdding.skyocean.SkyOcean
 import me.owdding.skyocean.api.SkyOceanItemId
+import me.owdding.skyocean.config.SkyOceanKeybind
 import me.owdding.skyocean.config.features.misc.MiscConfig
 import me.owdding.skyocean.data.profile.CraftHelperStorage.setSelected
+import me.owdding.skyocean.utils.ChatUtils.sendWithPrefix
 import me.owdding.skyocean.utils.Utils.contains
 import me.owdding.skyocean.utils.Utils.skyoceanReplace
 import net.minecraft.core.component.DataComponents
@@ -12,17 +16,21 @@ import net.minecraft.world.item.Items
 import net.minecraft.world.item.component.TooltipDisplay
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.screen.ScreenKeyReleasedEvent
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockapi.impl.tagkey.ItemTag
 import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
+import tech.thatgravyboat.skyblockapi.utils.extentions.getHoveredSlot
 import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.TextBuilder.append
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.bold
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
 @Module
 object RecipeModifier {
-
+    val SET_CRAFTHELPER_KEYBIND = SkyOceanKeybind("skyocean.keybind.crafthelper", InputConstants.KEY_V)
 
     @Subscription
     fun onInventoryChange(event: InventoryChangeEvent) {
@@ -54,6 +62,25 @@ object RecipeModifier {
                 McScreen.self?.let { it.resize(McClient.self, it.width, it.height) }
             }
         }
+    }
+
+    @Subscription
+    fun onKeybind(event: ScreenKeyReleasedEvent.Pre) {
+        if (!SET_CRAFTHELPER_KEYBIND.matches(event)) return
+
+        val reiHovered = REIRuntimeCompatability.getReiHoveredItemStack()
+        val mcScreenHovered = McScreen.asMenu?.getHoveredSlot()?.item?.takeUnless { it.isEmpty }
+        val item = mcScreenHovered ?: reiHovered ?: return
+
+        setSelected(SkyOceanItemId.fromItem(item))
+        McScreen.self?.let { it.resize(McClient.self, it.width, it.height) }
+
+        Text.of("Set selected Crafthelper item to ") {
+            append(item.cleanName) {
+                this.color = TextColor.GOLD
+                this.bold = true
+            }
+        }.sendWithPrefix()
     }
 
 }
