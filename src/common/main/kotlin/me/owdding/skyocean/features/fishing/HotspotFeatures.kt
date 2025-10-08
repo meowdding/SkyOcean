8db8b5b6befc238f622e68f245fc3f8b31d362f2
Empa <itsempa@users.noreply.github.com>
2025-10-08 09:32:55 +0200

    feat: also send title

--- src/common/main/kotlin/me/owdding/skyocean/features/fishing/HotspotFeatures.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/fishing/HotspotFeatures.kt
@@ -5,14 +5,18 @@ import me.owdding.skyocean.api.HotspotAPI
 import me.owdding.skyocean.config.features.fishing.HotspotFeaturesConfig
 import me.owdding.skyocean.events.fishing.HotspotEvent
 import me.owdding.skyocean.utils.Utils.text
+import me.owdding.skyocean.utils.chat.ChatUtils
 import me.owdding.skyocean.utils.chat.ChatUtils.sendWithPrefix
+import me.owdding.skyocean.utils.chat.OceanColors
 import me.owdding.skyocean.utils.rendering.RenderUtils.renderCircle
 import me.owdding.skyocean.utils.rendering.RenderUtils.renderCylinder
 import net.minecraft.util.ARGB
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderWorldEvent
+import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.helpers.McPlayer
+import tech.thatgravyboat.skyblockapi.utils.text.TextBuilder.append
 import tech.thatgravyboat.skyblockapi.utils.time.since
 import kotlin.time.Duration.Companion.seconds
 
@@ -61,8 +65,16 @@ object HotspotFeatures {
         if (distance > MIN_DISTANCE) return
         text {
             append(hotspot.type.displayComponent)
-            append(" Hotspot despawned!")
+            append(" Hotspot despawned!", OceanColors.WARNING)
         }.sendWithPrefix()
+        McClient.setTitle(
+            text {
+                append(ChatUtils.ICON_SPACE_COMPONENT)
+                append("Hotspot despawned!", OceanColors.WARNING)
+            },
+            stayTime = 3f,
+            fadeOutTime = 0.5f
+        )
     }
 
 }
