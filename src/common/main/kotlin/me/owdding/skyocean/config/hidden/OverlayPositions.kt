110013b4a63e58410bd6abafc331d6a35b043283
Mona <59416038+meowora@users.noreply.github.com>
2025-10-15 16:08:26 +0200

    Update src/common/main/kotlin/me/owdding/skyocean/config/hidden/OverlayPositions.kt
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyocean/config/hidden/OverlayPositions.kt
+++ src/common/main/kotlin/me/owdding/skyocean/config/hidden/OverlayPositions.kt
@@ -6,5 +6,5 @@ import me.owdding.lib.overlays.ConfigPosition
 object OverlayPositions : CategoryKt("overlays") {
     override val hidden: Boolean = true
 
-    val craftHelper by obj(ConfigPosition(2, 2))
+    val craftHelper by obj(ConfigPosition(5, 200))
 }
