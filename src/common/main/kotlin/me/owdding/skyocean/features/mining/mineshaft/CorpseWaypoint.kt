634a0cb3ab1225273a0be425105cf2a12f93a155
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-18 00:22:33 +0200

    refactor: code cleanup
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyocean/features/mining/mineshaft/CorpseWaypoint.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/mining/mineshaft/CorpseWaypoint.kt
@@ -1,17 +1,14 @@
 package me.owdding.skyocean.features.mining.mineshaft
 
-import com.google.gson.JsonArray
 import com.mojang.serialization.Codec
-import com.mojang.serialization.JsonOps
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import me.owdding.ktmodules.Module
 import me.owdding.skyocean.config.features.mining.MineshaftConfig
 import me.owdding.skyocean.utils.Utils
 import me.owdding.skyocean.utils.chat.ChatUtils
 import me.owdding.skyocean.utils.rendering.RenderUtils.renderTextInWorld
-import net.minecraft.util.ExtraCodecs
+import net.minecraft.core.BlockPos
 import net.minecraft.world.phys.Vec3
-import org.joml.Vector3f
 import tech.thatgravyboat.skyblockapi.api.area.mining.mineshaft.MineshaftAPI
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyIn
@@ -21,34 +18,7 @@ import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 @Module
 object CorpseWaypoint {
 
-    private var mineshaftCorpses: List<MineshaftCorpses> = emptyList()
-
-    private val CODEC = MineshaftCorpses.CODEC.listOf()
-
-    data class MineshaftCorpses(
-        val id: String,
-        val positions: List<Vector3f>,
-    ) {
-        companion object {
-            val CODEC = RecordCodecBuilder.create {
-                it.group(
-                    Codec.STRING.fieldOf("id").forGetter(MineshaftCorpses::id),
-                    ExtraCodecs.VECTOR3F.listOf().fieldOf("locations").forGetter(MineshaftCorpses::positions),
-                ).apply(it, ::MineshaftCorpses)
-            }
-        }
-    }
-
-    init {
-        Utils.loadFromRepo<JsonArray>("mining/shaft_corpses").let {
-            CODEC.parse(JsonOps.INSTANCE, it ?: JsonArray()).let {
-                if (it.isError) {
-                    throw RuntimeException(it.error().get().message())
-                }
-                mineshaftCorpses = it.getOrThrow()
-            }
-        }
-    }
+    private val mineshaftCorpses: List<MineshaftCorpses> = Utils.loadRepoData("mining/shaft_corpses", MineshaftCorpses.CODEC.listOf())
 
     @Subscription
     @OnlyIn(SkyBlockIsland.MINESHAFT)
@@ -58,6 +28,23 @@ object CorpseWaypoint {
 
         corpses?.positions?.forEach {
             event.renderTextInWorld(Vec3(it), ChatUtils.asSkyOceanColor("Corpse"), true)
+        }
+    }
+
+    data class MineshaftCorpses(
+        val id: String,
+        val positions: List<BlockPos>,
+    ) {
+        companion object {
+            val CODEC: Codec<MineshaftCorpses> = RecordCodecBuilder.create { record ->
+                record.group(
+                    Codec.STRING.fieldOf("id").forGetter(MineshaftCorpses::id),
+                    Codec.STRING.xmap(
+                        { it.split(",").map { it.toInt() }.let { BlockPos(it[0], it[1], it[2]) } },
+                        { "${it.x},${it.y},${it.z}" },
+                    ).listOf().fieldOf("locations").forGetter(MineshaftCorpses::positions),
+                ).apply(it, ::MineshaftCorpses)
+            }
         }
     }
 }
