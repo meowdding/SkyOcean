293d1f7043d404f3dd9c56418115794d99f1839d
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-18 01:04:16 +0200

    fix: corpse waypoints
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/skyocean/features/mining/mineshaft/CorpseWaypoint.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/mining/mineshaft/CorpseWaypoint.kt
@@ -1,7 +1,6 @@
 package me.owdding.skyocean.features.mining.mineshaft
 
 import com.mojang.serialization.Codec
-import com.mojang.serialization.codecs.RecordCodecBuilder
 import me.owdding.ktmodules.Module
 import me.owdding.skyocean.config.features.mining.MineshaftConfig
 import me.owdding.skyocean.utils.Utils
@@ -10,41 +9,47 @@ import me.owdding.skyocean.utils.rendering.RenderUtils.renderTextInWorld
 import net.minecraft.core.BlockPos
 import net.minecraft.world.phys.Vec3
 import tech.thatgravyboat.skyblockapi.api.area.mining.mineshaft.MineshaftAPI
+import tech.thatgravyboat.skyblockapi.api.area.mining.mineshaft.MineshaftType
+import tech.thatgravyboat.skyblockapi.api.area.mining.mineshaft.MineshaftVariant
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyIn
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderWorldEvent
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
+import tech.thatgravyboat.skyblockapi.utils.codecs.CodecUtils
+import tech.thatgravyboat.skyblockapi.utils.extentions.filterKeysNotNull
 
 @Module
 object CorpseWaypoint {
 
-    private val mineshaftCorpses: List<MineshaftCorpses> = Utils.loadRepoData("mining/shaft_corpses", MineshaftCorpses.CODEC.listOf())
+    private val CODEC = CodecUtils.map(
+        Codec.STRING.xmap(
+            { MineshaftType.fromId(it) },
+            { it?.id },
+        ),
+        CodecUtils.map(
+            Codec.STRING.xmap(
+                { MineshaftVariant.fromId(it) },
+                { it.id },
+            ),
+            Codec.STRING.xmap(
+                { it.split(",").map { it.toInt() }.let { BlockPos(it[0], it[1], it[2]) } },
+                { "${it.x},${it.y},${it.z}" },
+            ).listOf(),
+        ),
+    )
+
+    private val mineshaftCorpses: Map<MineshaftType, Map<MineshaftVariant, List<BlockPos>>> =
+        Utils.loadRepoData("mining/shaft_corpses", CODEC).filterKeysNotNull()
 
     @Subscription
     @OnlyIn(SkyBlockIsland.MINESHAFT)
     fun onRender(event: RenderWorldEvent.AfterTranslucent) {
         if (!MineshaftConfig.corpseWaypoint) return
-        val corpses = mineshaftCorpses.find { it.id == if (MineshaftAPI.isCrystal) "CRYSTAL" else MineshaftAPI.mineshaftType?.id }
+        val mineshaft = mineshaftCorpses.entries.find { it.key == MineshaftAPI.mineshaftType } ?: return
+        val corpses = mineshaft.value.entries.find { it.key == MineshaftAPI.mineshaftVariant }?.value ?: return
 
-        corpses?.positions?.forEach {
+        corpses.forEach {
             event.renderTextInWorld(Vec3(it), ChatUtils.asSkyOceanColor("Corpse"), true)
-        }
-    }
-
-    data class MineshaftCorpses(
-        val id: String,
-        val positions: List<BlockPos>,
-    ) {
-        companion object {
-            val CODEC: Codec<MineshaftCorpses> = RecordCodecBuilder.create { record ->
-                record.group(
-                    Codec.STRING.fieldOf("id").forGetter(MineshaftCorpses::id),
-                    Codec.STRING.xmap(
-                        { it.split(",").map { it.toInt() }.let { BlockPos(it[0], it[1], it[2]) } },
-                        { "${it.x},${it.y},${it.z}" },
-                    ).listOf().fieldOf("locations").forGetter(MineshaftCorpses::positions),
-                ).apply(it, ::MineshaftCorpses)
-            }
         }
     }
 }
