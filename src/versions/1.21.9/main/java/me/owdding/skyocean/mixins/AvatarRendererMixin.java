706a22af593ede20ae73176ad2249f976033686f
Mona <59416038+meowora@users.noreply.github.com>
2025-10-20 10:08:36 +0200

    chore: remove contrib stuff

--- src/versions/1.21.9/main/java/me/owdding/skyocean/mixins/AvatarRendererMixin.java
+++ src/versions/1.21.9/main/java/me/owdding/skyocean/mixins/AvatarRendererMixin.java
@@ -1,12 +1,9 @@
 package me.owdding.skyocean.mixins;
 
 import me.owdding.skyocean.accessors.AvatarRenderStateAccessor;
-import me.owdding.skyocean.utils.ContributorHandler;
 import net.minecraft.client.renderer.entity.player.AvatarRenderer;
 import net.minecraft.client.renderer.entity.state.AvatarRenderState;
-import net.minecraft.core.ClientAsset;
 import net.minecraft.world.entity.Avatar;
-import net.minecraft.world.entity.player.PlayerSkin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,20 +15,6 @@ public class AvatarRendererMixin {
     @Inject(method = "extractRenderState(Lnet/minecraft/world/entity/Avatar;Lnet/minecraft/client/renderer/entity/state/AvatarRenderState;F)V", at = @At("TAIL"))
     public void extractRenderState(Avatar abstractClientPlayer, AvatarRenderState playerRenderState, float f, CallbackInfo ci) {
         AvatarRenderStateAccessor.setUUID(playerRenderState, abstractClientPlayer.getUUID());
-
-        var contributor = ContributorHandler.INSTANCE.getContributors().get(abstractClientPlayer.getUUID());
-        if (contributor == null) {
-            return;
-        }
-
-        var skin = playerRenderState.skin;
-        playerRenderState.skin = new PlayerSkin(
-            skin.body(),
-            (ClientAsset.Texture) contributor.getCape(),
-            skin.elytra(),
-            skin.model(),
-            skin.secure()
-        );
     }
 
 }
