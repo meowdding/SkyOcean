1e901b0e327b881112d0dcbb4037789cca2007a8
Empa <itsempa@users.noreply.github.com>
2025-10-25 18:30:48 +0200

    fix: item highlight keybind not highlighting items in current inventory when pressed

--- src/common/main/kotlin/me/owdding/skyocean/features/item/search/highlight/ItemHighlighter.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/item/search/highlight/ItemHighlighter.kt
@@ -13,6 +13,7 @@ import me.owdding.skyocean.utils.Utils.skyoceanReplace
 import me.owdding.skyocean.utils.rendering.RenderUtils.renderBox
 import net.minecraft.core.BlockPos
 import net.minecraft.util.ARGB
+import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.InventoryTitle
@@ -25,7 +26,10 @@ import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland.PRIVATE_ISLAND
 import tech.thatgravyboat.skyblockapi.api.profile.items.storage.PlayerStorageInstance
 import tech.thatgravyboat.skyblockapi.api.profile.items.storage.StorageAPI
 import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.helpers.McPlayer
+import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockapi.impl.tagkey.ItemTag
+import tech.thatgravyboat.skyblockapi.mixins.accessors.ContainerScreenAccessor
 import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
 import tech.thatgravyboat.skyblockapi.utils.extentions.getSkyBlockId
 import java.util.concurrent.CopyOnWriteArrayList
@@ -92,6 +96,23 @@ object ItemHighlighter {
 
         if (sack.containingItems.any { filter.test(it) }) {
             event.item.highlight()
+        }
+    }
+
+    // TODO: add support for Sack/Storage highlighting when recalculating
+    fun recalculate() {
+        val filter = currentSearch ?: return
+        McPlayer.inventory.forEach {
+            if (filter.test(it)) it.highlight()
+        }
+        val menu = McScreen.asMenu ?: return
+        val slots = menu.menu.slots
+        for (slot in slots) {
+            val item = slot.item
+            if (filter.test(item)) {
+                item.highlight()
+                continue
+            }
         }
     }
 
