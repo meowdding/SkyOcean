54875d17387e0b43eb85aa6138efdc04702e519d
Mona <59416038+meowora@users.noreply.github.com>
2025-11-01 21:02:28 +0100

    fix: crafthelper issues with some recipes and id parsing

--- src/common/main/kotlin/me/owdding/skyocean/utils/extensions/MiscExtensions.kt
+++ src/common/main/kotlin/me/owdding/skyocean/utils/extensions/MiscExtensions.kt
@@ -3,3 +3,4 @@ package me.owdding.skyocean.utils.extensions
 import java.util.concurrent.atomic.AtomicReference
 
 fun <T> AtomicReference<T>.setUnlessNull(value: T?) = value?.let { set(it) }
+fun Int?.orElse(other: Int = 1) = this ?: other
