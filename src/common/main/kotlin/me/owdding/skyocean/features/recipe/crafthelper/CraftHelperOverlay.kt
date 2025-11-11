6cce6594bce6fc2646e44ebbb799d5149c46e546
Mona <59416038+meowora@users.noreply.github.com>
2025-11-11 22:41:22 +0100

    fix: crafthelper overlay showing outside skyblock

--- src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/CraftHelperOverlay.kt
+++ src/common/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/CraftHelperOverlay.kt
@@ -1,6 +1,7 @@
 package me.owdding.skyocean.features.recipe.crafthelper
 
 import earth.terrarium.olympus.client.ui.context.ContextMenu
+import kotlin.time.Duration.Companion.milliseconds
 import me.owdding.lib.builder.LayoutFactory
 import me.owdding.lib.displays.Displays
 import me.owdding.lib.displays.asWidget
@@ -17,13 +18,13 @@ import me.owdding.skyocean.utils.rendering.Overlay
 import me.owdding.skyocean.utils.rendering.SkyOceanOverlay
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.network.chat.Component
+import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.platform.drawSprite
 import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 import tech.thatgravyboat.skyblockapi.utils.extentions.translated
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
-import kotlin.time.Duration.Companion.milliseconds
 
 @Overlay
 object CraftHelperOverlay : SkyOceanOverlay() {
@@ -58,7 +59,7 @@ object CraftHelperOverlay : SkyOceanOverlay() {
     override val bounds: Pair<Int, Int> get() = (layout?.width?.plus(padding * 2) ?: 0) to (layout?.height?.plus(padding * 2) ?: 0)
     val state get() = CraftHelperManager.lastEvaluatedRoot.get()
 
-    override val enabled: Boolean get() = CraftHelperConfig.enableOverlay && state != null
+    override val enabled: Boolean get() = CraftHelperConfig.enableOverlay && state != null && LocationAPI.isOnSkyBlock
 
     override fun render(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float) {
         if (padding != 0) {
