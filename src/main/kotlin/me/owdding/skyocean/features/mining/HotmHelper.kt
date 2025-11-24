aae459137c849ef3b6c27c948d6dbc230d759b7f
Mona <59416038+meowora@users.noreply.github.com>
2025-11-24 09:37:39 +0100

    refactor: remove brackets

--- src/main/kotlin/me/owdding/skyocean/features/mining/HotmHelper.kt
+++ src/main/kotlin/me/owdding/skyocean/features/mining/HotmHelper.kt
@@ -118,7 +118,7 @@ object HotmHelper {
         val perkName = item.cleanName
         val perkByName = TreeRepoData.hotmByName(perkName) as? LevelingTreeNode ?: return
         val tooltipLines = item.getLore()
-        val isLocked = (item.getItemModel()) == Items.COAL
+        val isLocked = item.getItemModel() == Items.COAL
         val notEnoughPowder = tooltipLines.any { it.stripped.startsWith("you don't have enough ", true) }
         val level = tooltipLines.firstOrNull()?.let {
             val isBoosted = it.siblings.any { sibling -> sibling.style.color?.serialize() == "aqua" }
