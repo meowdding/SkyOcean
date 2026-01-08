0f295b48dc35c39f2ea55bbe5510c3ba79ee837c
Empa <itsempa@users.noreply.github.com>
2026-01-08 10:26:14 +0100

    chore: mona changes

--- src/main/java/me/owdding/skyocean/mixins/compat/rei/EntryHighlighterMixin.java
+++ src/main/java/me/owdding/skyocean/mixins/compat/rei/EntryHighlighterMixin.java
@@ -1,22 +1,21 @@
 package me.owdding.skyocean.mixins.compat.rei;
 
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
-import me.owdding.skyocean.features.item.search.highlight.ItemHighlighterREI;
+import me.owdding.skyocean.features.item.search.highlight.REIItemHighlighter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-// TODO: make it not a required mixin
 @Pseudo
 @IfModLoaded("roughlyenoughitems")
 @Mixin(targets = "me.shedaniel.rei.impl.client.gui.widget.EntryHighlighter")
 public class EntryHighlighterMixin {
 
     @SuppressWarnings("UnresolvedMixinReference")
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true, require = 0)
     private static void stopRender(CallbackInfo ci) {
-        if (ItemHighlighterREI.shouldStopREIHighlight()) ci.cancel();
+        if (REIItemHighlighter.shouldStopREIHighlight()) ci.cancel();
     }
 }
