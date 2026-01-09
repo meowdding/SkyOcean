52a87bccc5e7dbf73dd111328a37bb9cfe8fb255
Mona <59416038+meowora@users.noreply.github.com>
2026-01-09 16:21:36 +0100

    feat: load remote repo with delegates (#175)
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyocean/repo/misc/GalateaRepoData.kt
+++ src/main/kotlin/me/owdding/skyocean/repo/misc/GalateaRepoData.kt
@@ -3,20 +3,12 @@ package me.owdding.skyocean.repo.misc
 import me.owdding.ktcodecs.FieldName
 import me.owdding.ktcodecs.GenerateCodec
 import me.owdding.ktmodules.Module
-import me.owdding.lib.events.FinishRepoLoadingEvent
-import me.owdding.skyocean.utils.Utils
+import me.owdding.skyocean.utils.RemoteRepoDelegate
 import net.minecraft.core.BlockPos
-import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 
 @Module
 object GalateaRepoData {
-    var data: GalateaData? = null
-        private set
-
-    @Subscription(FinishRepoLoadingEvent::class)
-    fun onRepoLoad() {
-        data = Utils.loadFromRemoteRepo<GalateaData>("misc/galatea")
-    }
+    val data: GalateaData? by RemoteRepoDelegate.load("misc/galatea_data")
 }
 
 @GenerateCodec
