f8c04bca3f8d890051a9d3f6ec2d5deb262b773d
Helium9 <80259183+sivthepolarfox@users.noreply.github.com>
2026-01-31 23:26:06 +0100

    Use event :3

--- src/main/kotlin/me/owdding/skyocean/utils/PlayerUtils.kt
+++ src/main/kotlin/me/owdding/skyocean/utils/PlayerUtils.kt
@@ -1,43 +1,41 @@
 package me.owdding.skyocean.utils
 
 import me.owdding.ktmodules.Module
+import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.phys.Vec3
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.entity.EntityRemovedEvent
-import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
+import tech.thatgravyboat.skyblockapi.api.events.level.PacketReceivedEvent
 import tech.thatgravyboat.skyblockapi.helpers.McLevel
 import tech.thatgravyboat.skyblockapi.utils.time.currentInstant
-import java.util.UUID
+import java.util.*
 import kotlin.time.Instant
 
 @Module
 object PlayerUtils {
 
-    private var lastPos: MutableMap<UUID, Vec3> = mutableMapOf()
     private var lastMoveTime: MutableMap<UUID, Instant> = mutableMapOf()
 
     fun getLastMoveTime(uuid: UUID): Instant? = lastMoveTime[uuid]
 
-    @Subscription(TickEvent::class)
-    fun onTick() {
-        McLevel.players.forEach {
-            updatePlayerMovement(it)
-        }
+    @Subscription
+    fun onTick(event: PacketReceivedEvent) {
+        val packet = event.packet as? ClientboundPlayerPositionPacket ?: return
+        val player = McLevel.level.getEntity(packet.id) as? Player ?: return
+
+        val delta = packet.change.deltaMovement
+        updatePlayerMovement(player, delta)
     }
 
     @Subscription
     fun onEntityRemove(event: EntityRemovedEvent) {
         val player = event.entity as? Player ?: return
-        lastPos.remove(player.uuid)
         lastMoveTime.remove(player.uuid)
     }
 
-    private fun updatePlayerMovement(player: Player) {
-        val currentPos = player.position()
-        val lastPlayerPos = lastPos[player.uuid]
-        if (lastPlayerPos == null || currentPos != lastPlayerPos) {
-            lastPos[player.uuid] = currentPos
+    private fun updatePlayerMovement(player: Player, delta: Vec3) {
+        if (delta != Vec3.ZERO) {
             lastMoveTime[player.uuid] = currentInstant()
         }
     }
