02cbf6044b0b82a31e37729c26fa46b98bbbf732
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-03-01 22:42:33 +0100

    refactor: pretty sack notification colors
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyocean/features/chat/SackNotification.kt
+++ src/main/kotlin/me/owdding/skyocean/features/chat/SackNotification.kt
@@ -3,11 +3,14 @@ package me.owdding.skyocean.features.chat
 import me.owdding.ktmodules.Module
 import me.owdding.skyocean.config.features.chat.ChatConfig
 import me.owdding.skyocean.utils.chat.ChatUtils
+import me.owdding.skyocean.utils.chat.ChatUtils.sendWithPrefix
+import me.owdding.skyocean.utils.chat.OceanColors
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.hypixel.SacksChangeEvent
 import tech.thatgravyboat.skyblockapi.api.remote.RepoItemsAPI
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
 @Module
 object SackNotification {
@@ -21,12 +24,13 @@ object SackNotification {
         }.filter { it.first in ChatConfig.sackNotificationItems }
         if (amount.isEmpty()) return
 
-        val text = Text.join(
-            "Sack Notification | ",
-            amount.joinToString(", ") { "${it.first} x${it.second}" },
-        )
-
-        ChatUtils.chat(text)
+        Text.join(
+            "Sack Notification",
+            ChatUtils.SEPERATOR_COMPONENT,
+            Text.join(amount.map { Text.of("${it.first} x${it.second}", OceanColors.HIGHLIGHT) }, separator = Text.of(", "))
+        ) {
+            color = OceanColors.BASE_TEXT
+        }.sendWithPrefix()
     }
 
 }
