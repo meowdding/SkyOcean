574d2bf7ecfa19bcf739074db5389de6743b3f3f
Rain <178751120+Rainymei@users.noreply.github.com>
2026-05-06 15:29:58 +0200

    feat: add get from sack to pest traps (#220)
    * add onMouseClick event to InventorySideGui.kt
    add /gfs feature to PestBaitType.kt
    
    * use .apply instead of `.also layout`
    
    * refactor: use buttons instead of clickareas
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyocean/features/garden/PestBaitType.kt
+++ src/main/kotlin/me/owdding/skyocean/features/garden/PestBaitType.kt
@@ -1,12 +1,10 @@
 package me.owdding.skyocean.features.garden
 
-import earth.terrarium.olympus.client.components.buttons.Button
+import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
 import me.owdding.ktmodules.Module
+import me.owdding.lib.builder.DisplayFactory
 import me.owdding.lib.builder.LayoutFactory
-import me.owdding.lib.builder.MIDDLE
 import me.owdding.lib.displays.DisplayWidget
-import me.owdding.lib.displays.Displays
-import me.owdding.lib.displays.withPadding
 import me.owdding.skyocean.config.features.garden.GardenConfig
 import me.owdding.skyocean.helpers.InventorySideGui
 import me.owdding.skyocean.utils.Utils.unaryMinus
@@ -17,15 +15,17 @@ import tech.thatgravyboat.skyblockapi.api.area.farming.garden.pests.Spray
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerInitializedEvent
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+import me.owdding.lib.displays.Alignment as DisplayAlignment
 
 @Module
 object PestBaitType : InventorySideGui("(?:Pest|Mouse|Vermin) Trap", { GardenConfig.pestBaitAlignment }) {
 
     override val enabled: Boolean get() = GardenConfig.pestBaitType && SkyBlockIsland.GARDEN.inIsland()
 
-    override fun ContainerInitializedEvent.getLayout(): Layout = LayoutFactory.vertical {
+    override fun ContainerInitializedEvent.getLayout(): Layout = LayoutFactory.vertical(1) {
         horizontal {
             string(ChatUtils.ICON_SPACE_COMPONENT)
             string(-"garden.pest_bait_type")
@@ -33,21 +33,22 @@ object PestBaitType : InventorySideGui("(?:Pest|Mouse|Vermin) Trap", { GardenCon
 
         Spray.entries.forEach { spray ->
             val pests = Pest.getPests(spray)
-
-            horizontal(alignment = MIDDLE) {
-                display(Displays.item(spray.itemStack))
+            val display = DisplayFactory.horizontal(alignment = DisplayAlignment.CENTER) {
+                item(spray.itemStack)
                 textDisplay(": ${pests.joinToString(", ") { it.displayName }}") {
                     color = TextColor.DARK_GRAY
                 }
             }
-        }
 
-        val buttonDisplay = Displays.component(-"garden.open_sack").withPadding(2)
-        val sacksButton = Button().apply {
-            withRenderer(DisplayWidget.displayRenderer(buttonDisplay))
-            setSize(buttonDisplay.getWidth(), buttonDisplay.getHeight())
-            withCallback { McClient.sendCommand("/sacks") }
+            button {
+                withTexture(null)
+                withTooltip(Text.of("Click to get 64 ").append(spray.displayName).append(" from sacks!"))
+                withSize(display.getWidth(), display.getHeight())
+                withRenderer(DisplayWidget.displayRenderer(display))
+                withCallback {
+                    McClient.sendCommand("gfs ${spray.name} 64")
+                }
+            }
         }
-        widget(sacksButton)
     }
 }
