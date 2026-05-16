ad96aa499b25172d909b0af119f6bf9ffce400e6
Luna <lunaynx@gmail.com>
2026-05-16 15:02:29 +0200

    fix: Jerry's Workshop hotspots

--- src/main/kotlin/me/owdding/skyocean/api/HotspotAPI.kt
+++ src/main/kotlin/me/owdding/skyocean/api/HotspotAPI.kt
@@ -103,7 +103,7 @@ object HotspotAPI {
 
         val maxHotspotSize = when (LocationAPI.island) {
             SkyBlockIsland.CRIMSON_ISLE -> 25.0
-            else -> 9.0
+            else -> 16.0
         }
 
         for (entry in _hotspots.values) {
