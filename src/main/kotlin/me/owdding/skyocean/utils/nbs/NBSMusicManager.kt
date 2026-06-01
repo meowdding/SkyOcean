e07f721f2ca2de9873b7fe894a7e85d6e3568ba9
Mona <git@mona.cat>
2026-06-01 14:13:48 +0200

    chore: nbs test commands

--- src/main/kotlin/me/owdding/skyocean/utils/nbs/NBSMusicManager.kt
+++ src/main/kotlin/me/owdding/skyocean/utils/nbs/NBSMusicManager.kt
@@ -1,11 +1,39 @@
 package me.owdding.skyocean.utils.nbs
 
+import com.mojang.brigadier.arguments.StringArgumentType
+import me.owdding.ktmodules.Module
 import me.owdding.skyocean.SkyOcean
 import me.owdding.lib.utils.MeowddingLogger
+import me.owdding.skyocean.events.RegisterSkyOceanCommandEvent
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent.Companion.argument
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 
+@Module
 object NBSMusicManager: MeowddingLogger by SkyOcean.featureLogger("NBS Music Manager") {
     private val activePlayers = HashMap<String, NBSPlayer>()
+
+    @Subscription
+    context(event: RegisterSkyOceanCommandEvent)
+    fun commands() {
+        event.registerDev("nbs") {
+            thenCallback("start id", StringArgumentType.string()) {
+                play("command", argument<String>("id"))
+            }
+            thenCallback("stop") {
+                stop("command")
+            }
+            thenCallback("resume") {
+                resume("command")
+            }
+            thenCallback("pause") {
+                pause("command")
+            }
+            thenCallback("restart") {
+                restart("command")
+            }
+        }
+    }
 
     fun play(id: String, songResourceLocation: String) {
         stop(id)
