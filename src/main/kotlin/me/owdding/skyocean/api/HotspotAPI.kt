69b1848bd567e7d371fda61272dad637b9b9a2a5
Rhynohowl <toxicxkiller56@gmail.com>
2026-06-01 20:28:50 +0100

    bump sbapi version and expand maxHotspotSize for Atoll

--- src/main/kotlin/me/owdding/skyocean/api/HotspotAPI.kt
+++ src/main/kotlin/me/owdding/skyocean/api/HotspotAPI.kt
@@ -103,6 +103,7 @@ object HotspotAPI {
         val maxHotspotSize = when (LocationAPI.island) {
             SkyBlockIsland.CRIMSON_ISLE -> 25.0
             SkyBlockIsland.JERRYS_WORKSHOP -> 16.0
+            SkyBlockIsland.LOTUS_ATOLL -> 16.0
             else -> 9.0
         }
 
