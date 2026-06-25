c6fb1f1bc741d568eb3c825e92e088c7609c664d
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-06-25 17:13:19 +0200

    feat: forge reminder busy option
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyocean/helpers/CooldownHelper.kt
+++ src/main/kotlin/me/owdding/skyocean/helpers/CooldownHelper.kt
@@ -1,9 +1,12 @@
 package me.owdding.skyocean.helpers
 
 import me.owdding.ktmodules.Module
+import me.owdding.skyocean.config.features.mining.MiningConfig
+import tech.thatgravyboat.skyblockapi.api.area.dungeon.DungeonAPI
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.TimePassed
 import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
+import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.utils.extentions.currentInstant
 import tech.thatgravyboat.skyblockapi.utils.extentions.since
 import kotlin.time.Duration
@@ -12,8 +15,9 @@ import kotlin.time.Instant
 class CooldownHelper(
     val cooldown: () -> Duration,
     val onReady: () -> Unit,
+    val busyOption: () -> Boolean = { false },
 ) {
-    constructor(cooldown: Duration, onReady: () -> Unit) : this({ cooldown }, onReady)
+    constructor(cooldown: Duration, onReady: () -> Unit, busyOption: () -> Boolean = { false }) : this({ cooldown }, onReady, busyOption)
 
     private var lastUsed = Instant.DISTANT_PAST
 
@@ -38,10 +42,22 @@ class CooldownHelper(
         fun onTick() {
             cooldowns.forEach { helper ->
                 if (helper.lastUsed.since() >= helper.cooldown()) {
+                    if (helper.busyOption() && isBusy()) {
+                        return@forEach
+                    }
                     helper.onReady()
                     helper.setNow()
                 }
             }
+        }
+
+        private fun isBusy() = when {
+            SkyBlockIsland.DUNGEON_HUB.inIsland() -> !DungeonAPI.completed
+            SkyBlockIsland.MINESHAFT.inIsland() -> true
+            SkyBlockIsland.KUUDRA.inIsland() -> true
+            SkyBlockIsland.DARK_AUCTION.inIsland() -> true
+            SkyBlockIsland.THE_RIFT.inIsland() -> true
+            else -> false
         }
     }
 }
