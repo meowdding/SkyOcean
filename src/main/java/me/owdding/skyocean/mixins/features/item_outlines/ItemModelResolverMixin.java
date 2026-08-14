33340bd6e5c808c992343457c0660bbe9e6ccf7a
Mona <git@mona.cat>
2026-08-15 01:30:23 +0200

    refactor: holy formatting

--- src/main/java/me/owdding/skyocean/mixins/features/item_outlines/ItemModelResolverMixin.java
+++ src/main/java/me/owdding/skyocean/mixins/features/item_outlines/ItemModelResolverMixin.java
@@ -11,13 +11,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 @Mixin(ItemModelResolver.class)
 public class ItemModelResolverMixin {
     @Inject(
-        method = "appendItemLayers",
-        at = @At("HEAD")
+        method = "appendItemLayers", at = @At("HEAD")
     )
-    private void attachExtra(ItemStackRenderState output, ItemStack item, ItemDisplayContext displayContext, Level level, ItemOwner owner, int seed, CallbackInfo ci) {
+    private void attachExtra(
+        ItemStackRenderState output,
+        ItemStack item,
+        ItemDisplayContext displayContext,
+        Level level,
+        ItemOwner owner,
+        int seed,
+        CallbackInfo ci) {
         RarityOutlines.attachData(output, item);
     }
 }
