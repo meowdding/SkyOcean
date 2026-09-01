0bd9e0b886d44b81c2e5f10cbe7a3912bc19687e
JustAlex <justalex7473@gmail.com>
2026-09-01 16:31:52 -0400

    fix:use baseTranslation instead of constant

--- src/main/kotlin/me/owdding/skyocean/config/features/fishing/HotspotFeaturesConfig.kt
+++ src/main/kotlin/me/owdding/skyocean/config/features/fishing/HotspotFeaturesConfig.kt
@@ -7,62 +7,62 @@ import tech.thatgravyboat.skyblockapi.helpers.McClient
 
 object HotspotFeaturesConfig : ObjectKt() {
 
-    const val LANG_PATH = "skyocean.config.fishing.hotspot"
+    override val baseTranslation: String = "skyocean.config.fishing.hotspot"
 
     var warning by boolean(false) {
-        this.translation = "$LANG_PATH.warning"
+        this.translation = "warning"
     }
 
     init {
         separator {
-            this.title = "$LANG_PATH.highlight"
+            this.title = "highlight"
         }
     }
 
     var circleSurface by boolean(true) {
-        this.translation = "$LANG_PATH.circle_surface"
+        this.translation = "circle_surface"
     }
 
     var circleOutline by boolean(true) {
-        this.translation = "$LANG_PATH.circle_outline"
+        this.translation = "circle_outline"
     }
 
     var circleColumn by boolean(false) {
-        this.translation = "$LANG_PATH.circle_column"
+        this.translation = "circle_column"
     }
 
     var circleMatchesPlayerY by boolean(false) {
-        this.translation = "$LANG_PATH.circle_matches_player_y"
+        this.translation = "circle_matches_player_y"
     }
 
     var hideParticles by boolean(true) {
-        this.translation = "$LANG_PATH.hide_particles"
+        this.translation = "hide_particles"
     }
 
     var surfaceTransparency by transparency(50) {
-        this.translation = "$LANG_PATH.surface_transparency"
+        this.translation = "surface_transparency"
     }
 
     var outlineTransparency by transparency(100) {
-        this.translation = "$LANG_PATH.outline_transparency"
+        this.translation = "outline_transparency"
     }
 
     var columnTransparency by transparency(25) {
-        this.translation = "$LANG_PATH.column_transparency"
+        this.translation = "column_transparency"
     }
 
     init {
         separator {
-            this.title = "$LANG_PATH.announcements"
+            this.title = "announcements"
         }
     }
 
     var announce by enum(AnnouncementType.OFF) {
-        this.translation = "$LANG_PATH.announcements.announce"
+        this.translation = "announcements.announce"
     }
 
     var chatType by enum(ChatType.PARTY_ONLY) {
-        this.translation = "$LANG_PATH.announcements.chat_type"
+        this.translation = "announcements.chat_type"
     }
 
     enum class AnnouncementType {
--- src/main/kotlin/me/owdding/skyocean/config/features/fishing/WormholeFeaturesConfig.kt
+++ src/main/kotlin/me/owdding/skyocean/config/features/fishing/WormholeFeaturesConfig.kt
@@ -5,52 +5,52 @@ import me.owdding.skyocean.utils.transparency
 
 object WormholeFeaturesConfig : ObjectKt() {
 
-    const val LANG_PATH = "skyocean.config.fishing.wormhole"
+    override val baseTranslation: String = "skyocean.config.fishing.wormhole"
 
     var warning by boolean(false) {
-        this.translation = "$LANG_PATH.warning"
+        this.translation = "warning"
     }
 
     init {
         separator {
-            this.title = "$LANG_PATH.highlight"
+            this.title = "highlight"
         }
     }
 
     var circleSurface by boolean(true) {
-        this.translation = "$LANG_PATH.circle_surface"
+        this.translation = "circle_surface"
     }
 
     var circleOutline by boolean(true) {
-        this.translation = "$LANG_PATH.circle_outline"
+        this.translation = "circle_outline"
     }
 
     var circleColumn by boolean(false) {
-        this.translation = "$LANG_PATH.circle_column"
+        this.translation = "circle_column"
     }
 
     var circleMatchesPlayerY by boolean(false) {
-        this.translation = "$LANG_PATH.circle_matches_player_y"
+        this.translation = "circle_matches_player_y"
     }
 
 
     var hideParticles by boolean(true) {
-        this.translation = "$LANG_PATH.hide_particles"
+        this.translation = "hide_particles"
     }
 
     var color by color(0xAA00AA) {
-        this.translation = "$LANG_PATH.color"
+        this.translation = "color"
     }
 
     var surfaceTransparency by transparency(50) {
-        this.translation = "$LANG_PATH.surface_transparency"
+        this.translation = "surface_transparency"
     }
 
     var outlineTransparency by transparency(100) {
-        this.translation = "$LANG_PATH.outline_transparency"
+        this.translation = "outline_transparency"
     }
 
     var columnTransparency by transparency(25) {
-        this.translation = "$LANG_PATH.column_transparency"
+        this.translation = "column_transparency"
     }
 }
