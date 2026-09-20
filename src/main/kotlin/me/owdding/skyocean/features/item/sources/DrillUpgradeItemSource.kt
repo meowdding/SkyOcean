183c0daecd150d080dcb5e2defd77a235d968f99
Mona <git@mona.cat>
2026-09-20 18:22:43 +0200

    fix: some item source performance changes
    Closes: #307

--- src/main/kotlin/me/owdding/skyocean/features/item/sources/DrillUpgradeItemSource.kt
+++ src/main/kotlin/me/owdding/skyocean/features/item/sources/DrillUpgradeItemSource.kt
@@ -1,5 +1,6 @@
 package me.owdding.skyocean.features.item.sources
 
+import me.owdding.lib.utils.MemoizeUtil
 import me.owdding.skyocean.features.item.sources.system.ParentItemContext
 import me.owdding.skyocean.features.item.sources.system.SimpleTrackedItem
 import me.owdding.skyocean.utils.tags.SkyblockItemTagKey
@@ -7,15 +8,17 @@ import net.minecraft.network.chat.Component
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
 import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId
+import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId.Companion.getSkyBlockId
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
 object DrillUpgradeItemSource : ItemSource {
+    private val cache = MemoizeUtil.memoize { id: SkyBlockId -> id.cleanId in SkyblockItemTagKey.DRILLS  }
     override val type: ItemSources = ItemSources.DRILL_UPGRADE
     override fun getAll(): List<SimpleTrackedItem> = emptyList()
 
     override fun postProcess(items: List<SimpleTrackedItem>): List<SimpleTrackedItem> {
-        return items.filter { (itemStack) -> itemStack in SkyblockItemTagKey.DRILLS }
+        return items.filter { (itemStack) -> cache(itemStack.getSkyBlockId() ?: return@filter false) }
             .flatMap { item ->
                 val (itemStack) = item
                 listOfNotNull(
