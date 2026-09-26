3cbebe22cd5f4dfc8c2314865dad8f9668b14fa9
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-09-26 20:50:51 +0200

    chore: bump sbapi to fix breaking change crash :pleading_face:
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/modifiers/RecipeModifier.kt
+++ src/main/kotlin/me/owdding/skyocean/features/recipe/crafthelper/modifiers/RecipeModifier.kt
@@ -3,7 +3,6 @@ package me.owdding.skyocean.features.recipe.crafthelper.modifiers
 import me.owdding.skyocean.SkyOcean
 import me.owdding.skyocean.features.recipe.RecipeType
 import me.owdding.skyocean.features.recipe.RepoApiRecipe
-import me.owdding.skyocean.features.recipe.SkyOceanItemIngredient
 import me.owdding.skyocean.features.recipe.crafthelper.CraftHelperRecipe
 import me.owdding.skyocean.features.recipe.crafthelper.data.RepoLibRecipeTree
 import me.owdding.skyocean.features.recipe.mergeSameTypes
@@ -12,16 +11,11 @@ import me.owdding.skyocean.utils.Utils.contains
 import me.owdding.skyocean.utils.extensions.toIngredient
 import net.minecraft.world.item.Items
 import tech.thatgravyboat.repolib.api.recipes.CraftingRecipe
-import tech.thatgravyboat.repolib.api.recipes.ingredient.ItemIngredient
-import tech.thatgravyboat.skyblockapi.api.datatype.DataType
-import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
-import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId
 import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId.Companion.getSkyBlockId
 import tech.thatgravyboat.skyblockapi.impl.tagkey.ItemTag
 import tech.thatgravyboat.skyblockapi.utils.container.ContainerRegion
 import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
-import tech.thatgravyboat.skyblockapi.utils.extentions.get
 
 @CraftHelperModifier
 object RecipeModifier : AbstractCraftHelperModifier() {
